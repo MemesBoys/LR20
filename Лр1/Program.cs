22155cef36f3b2ab31e23ae812097fb9b4125e9f
dioxxide <87492545+MemesBoys@users.noreply.github.com>
2022-11-09 13:23:29 +0300

    Initial commit

---
+++ Лр1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Examole
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите сумму вклада = ");
+            float a = float.Parse(Console.ReadLine());
+            // Преобразуем строку в число
+            Console.Write("Введите процент по вкладу = ");
+            float b = float.Parse(Console.ReadLine());
+            // Преобразуем строку в число
+            Console.Write("Через год сумма на вкладе = {0:C2}", a + (a * (b / 100)));
+            // Считаем и выводим на экран
+            Console.ReadKey();
+        }
+    }
+}
---
+++ зад2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Examole
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите значения\nВершины А - x1 = ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.Write("y1 = ");
+            double y1 = double.Parse(Console.ReadLine());
+            // считываем значения
+            Console.Write("Вершины B - x2 = ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("y2 = ");
+            double y2 = double.Parse(Console.ReadLine());
+            // считываем значения
+            Console.Write("Вершины C - x3 = ");
+            double x3 = double.Parse(Console.ReadLine());
+            Console.Write("y3 = ");
+            double y3 = double.Parse(Console.ReadLine());
+
+            var a = Math.Sqrt((Math.Pow((x2 - x1), 2) + (Math.Pow((y2 - y1), 2))));
+            var b = Math.Sqrt((Math.Pow((x3 - x2), 2) + (Math.Pow((y3 - y2), 2))));
+            var c = Math.Sqrt((Math.Pow((x3 - x1), 2) + (Math.Pow((y3 - y1), 2))));
+            // Находим стороны
+            var p = (a + b + c)/2;
+            // находим полупериметр
+            double S = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+            Console.WriteLine("Площадь треугольника равна: {0:f2} ",S);
+            // высчитываем и выводим площадь
+            Console.ReadKey();
+        }
+    }
+}
